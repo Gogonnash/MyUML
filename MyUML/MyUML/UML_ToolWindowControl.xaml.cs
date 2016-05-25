117c9f0f742b0ac60eed6335e00572982692ac9b
Gogonnash <rschlegelberger@googlemail.com>
2016-05-25 16:38:10 +0200

    Imports und erster Zugriff auf den Syntaxtree

--- MyUML/MyUML/UML_ToolWindowControl.xaml.cs
+++ MyUML/MyUML/UML_ToolWindowControl.xaml.cs
@@ -6,7 +6,17 @@
 
 namespace MyUML
 {
+    using EnvDTE;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using Microsoft.VisualStudio.ComponentModelHost;
+    using Microsoft.VisualStudio.LanguageServices;
+    using Microsoft.VisualStudio.Shell;
+    using System;
     using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
     using System.Windows;
     using System.Windows.Controls;
 
@@ -32,9 +42,40 @@ namespace MyUML
         [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
         private void button1_Click(object sender, RoutedEventArgs e)
         {
+            Microsoft.CodeAnalysis.Document doc = GetActiveDocument();
+            Microsoft.CodeAnalysis.SyntaxTree syntaxTree;
+            doc.TryGetSyntaxTree(out syntaxTree);
+            SyntaxNode root = syntaxTree.GetRoot();
+            var children = root.ChildNodes();
+            var nameSpace = children.OfType<NamespaceDeclarationSyntax>().SingleOrDefault();
+            var classes = nameSpace.ChildNodes().OfType<ClassDeclarationSyntax>();
+
+            String output = "";
+            
+            foreach (var child in classes)
+            {
+                output = output + child.ToString() + " Typ:" + child.GetType().ToString() +  "\n\n";
+            }
+            
             MessageBox.Show(
-                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
-                "UML_ToolWindow");
+                output,
+                "Projektname: " + doc.Project.Name);
+        }
+
+
+        private Microsoft.CodeAnalysis.Document GetActiveDocument()
+        {
+            var dte = Package.GetGlobalService(typeof(DTE)) as DTE;
+            var activeDocument = dte?.ActiveDocument;
+            if (activeDocument == null) return null;
+
+            var componentModel = (IComponentModel)Package.GetGlobalService(typeof(SComponentModel));
+            var workspace = (Workspace)componentModel.GetService<VisualStudioWorkspace>();
+
+            var documentid = workspace.CurrentSolution.GetDocumentIdsWithFilePath(activeDocument.FullName).FirstOrDefault();
+            if (documentid == null) return null;
+
+            return workspace.CurrentSolution.GetDocument(documentid);
         }
     }
 }
